1654791c319071238db1de6a4334adf3fe27e219
Fiz5892 <156051961+Fiz5892@users.noreply.github.com>
2025-07-05 16:46:11 +0700

    header and home section

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'POSITRON 2025 - Electrical Unity')
+
+@section('content')
+
+    <!-- Hero Section -->
+    <section id="home" class="home-section">
+        <!-- Hero Content -->
+        <div class="hero-content">
+            <!-- Logo -->
+            <!-- Main Title dengan font Back to School -->
+            <div class="block">
+                <h1 class="hero-title back-to-school-font">POSITRON</h1>
+                <h1 class="hero-title atlanta-font">2025</h1>
+            </div>
+
+
+            <div class="hero-logo"></div>
+        </div>
+    </section>
+
+@endsection
