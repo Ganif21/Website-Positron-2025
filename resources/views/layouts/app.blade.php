eba9cc0221257180a6748f793acba0eacc7fa703
Fiz5892 <156051961+Fiz5892@users.noreply.github.com>
2025-07-06 13:57:43 +0700

    add slider and timer section

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="{{ asset('css/font.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300;400;600;700&display=swap"
         rel="stylesheet" />
     @yield('styles') <!-- Tambahkan baris ini -->
